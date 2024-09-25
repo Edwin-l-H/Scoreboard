a0bd926c78289c50fdf103bc4c8b9df6806bc7c2
Edwin-l-H <edwinlh2789@nycstudents.net>
2024-09-25 13:34:18 +0000

    Pending changes exported from your codespace

---
+++ Scoreboard.java
@@ -0,0 +1,18 @@
+public class Scoreboard
+{
+    private String teamOne;
+    private String teamTwo;
+    private int teamoneScore;
+    private int teamtwoScore;
+    private String activeTeam;
+
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+    }
+    public String getScore()
+    {
+        return null;
+    }
+}
