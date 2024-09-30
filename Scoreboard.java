6d7eee60a0d6884b7f1f5c852df8889e32f504c9
Edwin-l-H <edwinlh2789@nycstudents.net>
2024-09-30 09:23:13 -0400

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -14,19 +14,36 @@ public class Scoreboard
     }
     public String getScore()
     {
-        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+        return teamOneScore + " - " + teamTwoScore + " - " + activeTeam;
     }
 
     public void recordPlay(int score)
     {
-        if (activeTeam == teamOne) 
+        //This bit of code adds the score earned from the play
+        if (activeTeam == teamOne)
         {
-            teamOneScore+= score;
+            teamOneScore += score;
         }
-        
-        if (activeTeam == teamTwo) 
+        else
         {
             teamTwoScore += score;
         }
-    } 
+
+
+        //This bit of code switches the active team
+        if (score <= 0)
+        {
+            
+            if (activeTeam == teamOne) 
+            {
+                activeTeam = teamTwo;
+            }
+            else if (activeTeam == teamTwo)
+            {
+                activeTeam = teamOne;
+            }
+        }
+        
+
+    }
 }
