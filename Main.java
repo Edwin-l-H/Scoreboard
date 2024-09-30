ea544861bc761bd7014273df81e6b1ca29010c0e
Edwin-l-H <edwinlh2789@nycstudents.net>
2024-09-30 09:21:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,56 @@ public class Main
     {
         String info;
         Scoreboard game = new Scoreboard("Red", "Blue");
-
-        game.recordPlay(1);
-
         info = game.getScore();
-        
         System.out.println(info);
 
+        //Team 1 gains a point.
+        game.recordPlay(1);
+        info = game.getScore();
+        System.out.println(info);
 
+        //Team 1 does not gain a point. Active team changes to team 2.
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+
+        //Team 2 gains 3 points.
+        game.recordPlay(3);
+        info = game.getScore();
+        System.out.println(info);
+
+        //Team 2 gains 1 point.
+        game.recordPlay(1);
+        info = game.getScore();
+        System.out.println(info);
+
+        //Team 2 does not gain a point. Active team changes to team 2.
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+
+        //Team 1 does not gain a point. Active team changes to team 2.
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+
+        //Team 2 gains 4 points.
+        game.recordPlay(4);
+        info = game.getScore();
+        System.out.println(info);
+
+        //Team 2 does not gain a point. Active team changes to team 2.
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+
+        //This is a different game being played
+        Scoreboard match = new Scoreboard("Lions", "Tigers");
+
+        info = match.getScore();
+        System.out.println(info);
+
+        info = game.getScore();
+        System.out.println(info);
     }
 }
